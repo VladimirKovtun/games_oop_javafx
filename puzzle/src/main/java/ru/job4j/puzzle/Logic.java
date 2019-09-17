9afa16c089f1fd0ba718eaa96e1b8a1258335863
VladimirKovtun <vovkammmorkovka@gmail.com>
2019-09-18 02:48:46 +0300

    Решение задачи 'Сокобан. Тестовое задание.'[#178629]

--- puzzle/src/main/java/ru/job4j/puzzle/Logic.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -71,6 +71,24 @@ public class Logic {
     public boolean isWin() {
         int[][] table = this.convert();
         boolean result = false;
+        for (int i = 0; i < table.length; i++) {
+            if (table[i][i] == 1) {
+                int count1 = 0;
+                int count2 = 0;
+                for (int j = 0; j < table.length; j++) {
+                    if (table[i][j] == 1)
+                        count1++;
+                }
+                for (int j = 0; j < table.length; j++) {
+                    if (table[j][i] == 1)
+                        count2++;
+                }
+                if (table.length == count1 || table.length == count2) {
+                    result = true;
+                    break;
+                }
+            }
+        }
         return result;
     }
 
